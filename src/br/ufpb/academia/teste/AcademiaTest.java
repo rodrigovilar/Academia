69054f3866c989e89a40eae7c375733a8d696261
heitorjose <heitorcarrazzoni@gmail.com>
2014-03-13 16:16:43 -0300

    Teste AdcionarAluno

--- src/br/ufpb/academia/teste/AcademiaTest.java
+++ src/br/ufpb/academia/teste/AcademiaTest.java
@@ -6,16 +6,37 @@ import org.junit.Test;
 
 import br.ufpb.academia.fachada.FachadaAcademia;
 import br.ufpb.academia.model.Aluno;
+import br.ufpb.academia.model.Endereco;
+import br.ufpb.academia.model.Modalidade;
 
 public class AcademiaTest {
 	
-	private FachadaAcademia academia;
+	private FachadaAcademia academia = new FachadaAcademia();
 	
 	@Test
-	public void test() {
+	public void AdcionarAluno() {
 		Aluno aluno = new Aluno();
-				
-		academia.adcionarAluno(aluno);
+		Endereco end = new Endereco();
+		Modalidade modalidade = new Modalidade();
+	    aluno.setNome("Heitor");
+	    aluno.setEmail("heitor@gmail.com");
+	    aluno.setMatricula(1);
+	    aluno.setTelefone("2356450");
+	    modalidade.setNome("musculação");
+	    modalidade.setValor(70);
+	    end.setRua("Otavio Felix");
+	    end.setNumero("101");
+	    end.setComplemento("Apt 103");
+	    end.setBairro("jose americo de Almeida");
+	    end.setCidade("Joao Pessoa");
+	    end.setEstado("PB");
+	    aluno.setEndereco(end);
+	    aluno.setModalidade(modalidade);
+	    academia.adcionarAluno(aluno);
+	    
+	    
+	    
+	    
 	}
 	
 	
