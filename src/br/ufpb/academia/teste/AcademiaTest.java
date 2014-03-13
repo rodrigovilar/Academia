9dc2d1f411d96d15730267e18020d66e0925a60c
Mailton <Mailton@Mailton-PC>
2014-03-13 15:59:25 -0300

    primeiro teste. (aqui não passou!)  kkkkk

--- src/br/ufpb/academia/teste/AcademiaTest.java
+++ src/br/ufpb/academia/teste/AcademiaTest.java
@@ -5,12 +5,19 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import br.ufpb.academia.fachada.FachadaAcademia;
+import br.ufpb.academia.model.Aluno;
 
 public class AcademiaTest {
 	
+	private FachadaAcademia academia;
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Aluno aluno = new Aluno();
+				
+		academia.adcionarAluno(aluno);
 	}
+	
+	
 
 }
