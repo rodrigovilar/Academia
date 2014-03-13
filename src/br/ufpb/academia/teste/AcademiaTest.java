056e232a2b9150a2e02cc95b02ed86adc058ca5d
Mailton <Mailton@Mailton-PC>
2014-03-13 15:45:15 -0300

    Criação do Pacote de Teste;

---
+++ src/br/ufpb/academia/teste/AcademiaTest.java
@@ -0,0 +1,16 @@
+package br.ufpb.academia.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import br.ufpb.academia.fachada.FachadaAcademia;
+
+public class AcademiaTest {
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
